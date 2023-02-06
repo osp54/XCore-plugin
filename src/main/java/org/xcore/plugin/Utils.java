0371900271a25d6558b6309048cd10a11dded030
OSP <76648940+osp54@users.noreply.github.com>
2023-02-06 19:18:29 +0200

    fix

--- src/main/java/org/xcore/plugin/Utils.java
+++ src/main/java/org/xcore/plugin/Utils.java
@@ -15,20 +15,18 @@ import static mindustry.Vars.maps;
 public class Utils {
     public static String getLeaderboard() {
         var builder = new StringBuilder();
-        Seq<PlayerData> sorted = Database.cachedPlayerData.copy().values().toSeq().sort(d -> d.rating);
+        Seq<PlayerData> sorted = Database.cachedPlayerData.copy().values().toSeq().filter(d -> d.rating != 0).sort(d -> d.rating).reverse();
         sorted.truncate(10);
 
         builder.append("[blue]Leaderboard\n\n");
         for (int i = 0; i < sorted.size; i++) {
             var data = sorted.get(i);
-            if (data.rating != 0) {
-                builder.append("[orange]").append(i + 1)
-                        .append(". ")
-                        .append(data.nickname)
-                        .append(":[cyan] ")
-                        .append(data.rating).append(" []rating\n");
+            builder.append("[orange]").append(i + 1)
+                    .append(". ")
+                    .append(data.nickname)
+                    .append(":[cyan] ")
+                    .append(data.rating).append(" []rating\n");
             }
-        }
 
         return builder.toString();
     }
